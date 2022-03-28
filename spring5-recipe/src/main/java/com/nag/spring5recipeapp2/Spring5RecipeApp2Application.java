e7ab3099e50ee7152dfa43ccf48bac221564f0c8
desaraju02 <desaraju02@gmail.com>
2022-03-28 11:38:47 +0530

    First Commit

---
+++ spring5-recipe/src/main/java/com/nag/spring5recipeapp2/Spring5RecipeApp2Application.java
@@ -0,0 +1,13 @@
+package com.nag.spring5recipeapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeApp2Application.class, args);
+    }
+
+}
---
+++ spring5-recipe/src/test/java/com/nag/spring5recipeapp2/Spring5RecipeApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nag.spring5recipeapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
